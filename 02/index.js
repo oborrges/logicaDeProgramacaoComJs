4dd53d49b2eb28c368ac46679e4c4387db555285
Luis Borges <oborrges@gmail.com>
2023-04-24 17:08:12 -0300

    sum JS

---
+++ 02/index.js
@@ -0,0 +1,20 @@
+/* 
+solicite dois números
+faça a soma deles
+apresente o resultado ao usuario
+*/
+
+/*
+alert("Vamos somar dois números?")
+let firstNumber = prompt("Digite um número:")
+let secondNumber = prompt("Digite outro número:")
+alert("O resultado é: " + (Number(firstNumber) + Number(secondNumber)))
+*/
+
+// Fórma semantica
+
+alert("Vamos somar dois números?")
+let firstNumber = prompt('Digite o primeiro número:')
+let secondNumber = prompt('Digite o segundo número:')
+let result = Number(firstNumber) + Number(secondNumber)
+alert("O resultado final é: " + result)
---
+++ 02/notes.md
@@ -0,0 +1,13 @@
+Boas praticas na escrita de variáveis
+
+[] sem espaço ou acentuações especiais no nome
+[] em inglês
+[] não pode iniciar com números
+[] camelCase
+
+[] tipo de dados: number
+[] operador matemático
+[] manipulação de dados
+  []type conversion (type casting)
+[] group operator 
+
